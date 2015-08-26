c21eb3a8be0489250c131a8ead969bd82bce4560
Marie CUER <mcuer@e3r3p1.42.fr>
2015-08-26 09:55:33 +0200

    test

---
+++ ulstr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ulstr.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcuer <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/25 12:14:59 by mcuer             #+#    #+#             */
+/*   Updated: 2015/08/25 17:01:01 by mcuer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putchar(char c)
+{
+	write(1 ,&c ,1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			ft_putchar(str[i] - 32);
+		}
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			ft_putchar(str[i] + 32);
+		}
+		i++;
+	}
+}
+
+int		main(int argc,char **argv)
+{
+	if (argc < 1 )
+	{
+		ft_putchar('\n');
+		return (-1);
+	}
+	ft_putstr(argv[1]);
+	return (0);
+}
